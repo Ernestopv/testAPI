89b8535da1bab39637437c91da8933904a546eb1
Ernesto Prado <ernesto.prado.v@gmail.com>
2021-11-08 03:15:32 +0000

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using testAPI.Models;
+
+namespace testAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+
+        [HttpGet("token")]
+        public IActionResult GetToken()
+        {
+            var unixTime = ((DateTimeOffset)DateTime.UtcNow.AddDays(1)).ToUnixTimeSeconds();
+
+            var roles = new List<string>() { "admin", "single", "yahoo", "BlablablaRights" };
+            var accessToken = new AccessToken()
+            {
+                aud = "http://ernesto.com",
+                iss = "https://krakend.io",
+                sub = "1234567890qwertyuio",
+                jti = "mnb23vcsrt756yuiomnbvcx98ertyuiop",
+                roles = roles,
+                exp = unixTime
+            };
+
+
+
+            var refreshToken = new RefreshToken()
+            {
+                aud = "http://ernesto.com",
+                iss = "https://krakend.io",
+                sub = "1234567890qwertyuio",
+                jti = "mnb23vcsrt756yuiomnbvcx98ertyuiop",
+                exp = unixTime
+            };
+
+
+            var response = new Response()
+            {
+               
+                access_token = accessToken,
+                refresh_token = refreshToken,
+                exp = unixTime
+            };
+
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testAPI.Models
+{
+    public class Response
+    {
+        public AccessToken access_token { get; set; }
+
+        public RefreshToken refresh_token { get; set; }
+
+        public long exp { get; set; }
+    }
+
+
+    public class AccessToken
+    {
+        public string aud { get; set; }
+        public string iss { get; set; }
+        public string sub { get; set; }
+        public string jti { get; set; }
+        public List<string> roles { get; set; }
+
+        public  long  exp { get; set; }
+
+    }
+
+
+    public class RefreshToken
+    {
+        public string aud { get; set; }
+        public string iss { get; set; }
+        public string sub { get; set; }
+        public string jti { get; set; }
+
+        public long exp { get; set; }
+
+    }
+}
